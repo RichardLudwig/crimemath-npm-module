837648c4b8c194ab42e4e1e6d64ce939d5c5a59e
Richard Ludwig <richard@richardludwig.com>
2019-12-25 10:53:01 -0800

    added header for generic criminology formulas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // This package contains mathematical formulas, for users to quickly calculate outputs they need for criminological/criminalistic/public safety research purposes. This package is geared towards students studying these topics but anyone is welcome to use this package. There will simply be a learning curve if you are no used to using math in this way.
 
-// Consult the technical documentation to learn how to "call" these functions, and contact me at my website (https://richardludwig.com) if there is a formula you would like me to add to the module.
+// Consult the technical documentation (coming soon) to learn how to "call" these functions, and contact me at my website (https://richardludwig.com) if there is a formula you would like me to add to the module.
 
+// GENERAL CRIMINOLOGY FORMULAS
 // Crime Rate Formula
 exports.crimeRate = (crimesReported, population) => {
   return (crimesReported / population) * 100000;
